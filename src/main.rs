6567a4588bf581be621391e17c4419c3718810d1
Sam Vevang <sam.vevang@gmail.com>
2017-12-28 20:50:12 -0600

    WIP convert geojson blobs

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ extern crate git2;
 extern crate time;
 
 use std::str;
+use std::path::Path;
+
 use docopt::Docopt;
 use git2::{Repository, ObjectType};
 use git2::{Error};
@@ -30,21 +32,25 @@ struct Args {
 
 fn run(args: &Args) -> Result<(), Error> {
     let path = args.flag_git_dir.as_ref().map(|s| &s[..]).unwrap_or(".");
-    println!("path: {}", path);
-    let repo = try!(Repository::open(path));
+    let repo = Repository::open(path)?;
 
-    println!("wow looking at all the files {}", repo.path().to_string_lossy());
+    println!("looking for wof geojson in {}", repo.path().to_string_lossy());
 
     let index = repo.index().unwrap();
 
-    //index.read(true);
-
     for index_entry in index.iter() {
-        let object = repo.find_object(index_entry.id, Some(ObjectType::Blob));
-        println!("path: {} OID: {}", String::from_utf8(index_entry.path).unwrap(), index_entry.id);
+        let path_name = String::from_utf8(index_entry.path).unwrap();
+        println!("path name {}", path_name);
+        println!("looking!");
+        let object = match repo.find_object(index_entry.id, Some(ObjectType::Blob)) {
+            Ok(i) => i,
+            Err(err) => continue,
+        };
+        let blob = object.as_blob();
+        //println!("path: {} OID: {}", String::from_utf8(index_entry.path).unwrap(), index_entry.id);
+        let path = Path::new(&path_name);
+        //path.display();
     }
-
-
 
     Ok(())
 }
