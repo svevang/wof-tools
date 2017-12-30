05ad4b5b95c5a045fcf74511e0baf3347fbb9a38
Sam Vevang <sam.vevang@gmail.com>
2017-12-30 10:10:59 -0600

    use the git index to speed up file traversal

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,17 @@
 extern crate serde_derive;
 extern crate docopt;
 extern crate time;
-extern crate walkdir;
 extern crate rayon;
+extern crate git2;
 
 use std::str;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 use docopt::Docopt;
-
-use walkdir::WalkDir;
-
+use git2::{Repository};
 use rayon::prelude::*;
 
 #[derive(Deserialize)]
@@ -23,19 +22,24 @@ struct Args {
 
 fn run(args: &Args) -> Result<(), Box<Error>> {
     let wof_path = args.flag_wof_data_dir.as_ref().map(|s| &s[..]).unwrap();
-    println!("loading geojson from {}", wof_path);
+    println!("loading geojson from repository located around {}", wof_path);
 
-    let paths: Vec<_> = WalkDir::new(wof_path)
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|f| f.ok())       // filter out errors (silently!)
-        .map(|f| f.path().to_owned()) // take the path and take ownership
+    let repo = Repository::open(wof_path)?;
+    let index = repo.index()?;
+
+    let paths: Vec<_> = index.iter()
+        .map(|index_entry| { 
+            String::from_utf8(index_entry.path).unwrap()
+         })
         .collect();                   // collect into whatever list is
 
-    paths.par_iter().for_each (|file_path| {
+    println!("paths len:{}", paths.len());
+    paths.par_iter().for_each (|path| {
+        let file_path = Path::new(wof_path).join(&path);
 
+        println!("lookin at: {} ..  file? {}", file_path.display(), file_path.is_file());
         if file_path.is_file() {
-            let mut file = File::open(file_path).unwrap();
+            let mut file = File::open(&file_path).unwrap();
             let mut contents = Vec::new();
             file.read_to_end(&mut contents).unwrap();
             println!("{} length: {}", file_path.display(), contents.len() );
